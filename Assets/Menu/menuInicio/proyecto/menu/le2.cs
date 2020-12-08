6525e3b93a0452eb2e262201a9f978d543a12257
rogerbenanny <rogercjunomerma_5555@hotmail.com>
2020-12-08 17:34:34 -0500

    escena niveles

---
+++ Assets/Menu/menuInicio/proyecto/menu/le2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class le2 : MonoBehaviour
+{
+    public void Empe()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Cerr()
+    {
+        Application.Quit();
+        Debug.Log("Salir");
+    }
+}
